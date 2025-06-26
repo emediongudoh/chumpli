9d7ffa2ebacdb272ae14b820d5636f42b340644f
Emediong Udoh <emediong5135@gmail.com>
2025-06-26 20:39:18 +0100

    Setup Prisma with Neon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import { ModeToggle } from "@/components/mode-toggle";
-import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  const users = await prisma.user.findMany();
+
   return (
-    <div>
-      <Button>Click me</Button>
-      <ModeToggle />
+    <div className="-mt-16 flex min-h-screen flex-col items-center justify-center">
+      <h1 className="mb-8 text-4xl font-bold">Superblog</h1>
+      <ol className="list-inside list-decimal">
+        {users.map(user => (
+          <li
+            key={user.id}
+            className="mb-2"
+          >
+            {user.name}
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
