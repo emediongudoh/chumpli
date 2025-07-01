a96da69f5d145a9fb5676472029c3035194d40ba
Emediong Udoh <emediong5135@gmail.com>
2025-07-01 16:50:46 +0100

    Setup Prisma with Neon DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
+export default async function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="-mt-16 flex min-h-screen flex-col items-center justify-center">
+      <h1 className="mb-8 font-[family-name:var(--font-geist-sans)] text-4xl font-bold text-[#333333]">
+        Chumpli
+      </h1>
+      <ol className="list-inside list-decimal font-[family-name:var(--font-geist-sans)]">
+        <li className="mb-2">Alice</li>
+        <li>Bob</li>
+      </ol>
     </div>
   );
 }
